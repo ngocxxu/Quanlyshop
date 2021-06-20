8b7cfbc1e8ddb747b2224a4dd30d0ad67df254fd
ngocxxu <ngocquach43@gmail.com>
2021-06-20 10:11:07 +0700

    version2_vietChucNangRenderProduct

--- controllers/index.js
+++ controllers/index.js
@@ -1,1 +1,27 @@
 console.log('Hello');
+
+var arrProduct=[
+    {id:1,name:'iphone',price: 1000, img:'https://picsum.photos/id/1/200/200'},
+    {id:2,name:'iphone xs',price: 2000, img:'https://picsum.photos/id/2/200/200'},
+    {id:3,name:'iphone xs max',price: 3000, img:'https://picsum.photos/id/3/200/200'},
+]
+
+function renderProduct(){
+    var content='';
+    for(var i=0; i<arrProduct.length; i++){
+        let product = arrProduct[i];
+        content += `
+        <div class="col-4">
+            <div class="card">
+                <img src="${product.img}">
+                <div class="card-body">
+                    <p>${product.price}</p>
+                    <button>Mua hàng</button>
+                </div>
+            </div>
+        </div>`
+
+    }
+    document.querySelector('#arrProduct').innerHTML= content;
+}
+renderProduct();
